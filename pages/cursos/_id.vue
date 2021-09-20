8e051cc58eeaf3bfef4a66e62c8d25930ec14f20
vitorcosta0399 <vitor@nerau.com.br>
2021-09-20 20:15:07 -0300

    detalhes

---
+++ pages/cursos/_id.vue
@@ -0,0 +1,158 @@
+<template>
+  <section class="details">
+    <div class="container">
+      <div class="details__left">
+        <nuxt-link to="/cursos">
+          <OutlineChevronLeftIcon /> Voltar para os cursos
+        </nuxt-link>
+        <div class="details__left-content">
+          <h2>Título do curso</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            varius nisi sed urna semper, ut dictum enim finibus. Praesent lacus
+            dui, vulputate ac mauris eu, ultricies vehicula leo. Nullam
+            condimentum eget dolor ut blandit. Cras nunc diam, maximus sed
+            pellentesque id, facilisis at ligula. Integer rhoncus felis vitae
+            massa bibendum, in tristique risus interdum. Curabitur sit amet
+            venenatis justo, a accumsan nisi. Nullam porttitor lobortis nisl, at
+            fermentum ante suscipit in. Sed nec eleifend lacus. Mauris a erat
+            purus. Curabitur tempus lacinia luctus. Aenean erat quam, aliquet
+            nec mauris at, egestas tempor erat.
+          </p>
+
+          <ul>
+            <li>• Item que o curso aborda</li>
+            <li>• Item que o curso aborda</li>
+            <li>• Item que o curso aborda</li>
+            <li>• Item que o curso aborda</li>
+            <li>• Item que o curso aborda</li>
+            <li>• Item que o curso aborda</li>
+            <li>• Item que o curso aborda</li>
+            <li>• Item que o curso aborda</li>
+            <li>• Item que o curso aborda</li>
+            <li>• Item que o curso aborda</li>
+            <li>• Item que o curso aborda</li>
+            <li>• Item que o curso aborda</li>
+            <li>• Item que o curso aborda</li>
+            <li>• Item que o curso aborda</li>
+            <li>• Item que o curso aborda</li>
+            <li>• Item que o curso aborda</li>
+          </ul>
+
+          <div class="embed-responsive">
+            <iframe
+              class="embed-responsive-item"
+              src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0"
+              allowfullscreen
+            ></iframe>
+          </div>
+        </div>
+      </div>
+
+      <div class="details__right">
+        <div class="details__top">
+          <OutlineLockClosedIcon />
+          <p>
+            Assine nossa plataforma para obter acesso a esse conteúdo e muitos
+            outros
+          </p>
+
+          <a-button type="primary">ASSINE AGORA</a-button>
+
+          <small>OU</small>
+          <p>Se você ja for assinante, faça login para acessar ao curso</p>
+          <a-button type="primary">LOGIN</a-button>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  layout: "notLogged",
+};
+</script>
+
+<style lang="scss" scoped>
+.details {
+  margin-bottom: 32px;
+
+  .container {
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
+  }
+
+  .details__left {
+    width: calc(60% - 15px);
+    a {
+      display: flex;
+      align-items: center;
+      border: 0;
+      background: none;
+      color: $primary-yellow;
+      cursor: pointer;
+      padding: 8px 0;
+      margin: 16px 0;
+
+      svg {
+        width: 24px;
+        margin-right: 8px;
+      }
+    }
+
+    .details__left-content {
+      h2 {
+        font-weight: bold;
+        font-size: 1.5rem;
+        margin-bottom: 8px;
+      }
+
+      ul {
+        column-count: 2;
+        margin-top: 32px;
+      }
+
+      p,
+      li {
+        font-size: 0.875rem;
+      }
+
+      .embed-responsive {
+        position: relative;
+        max-width: 100%;
+        width: 100%;
+        padding-top: 56.25%;
+        margin-top: 32px;
+
+        iframe {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          border: 0;
+          border-radius: 16px;
+        }
+      }
+    }
+  }
+
+  .details__right {
+    margin-top: 40px;
+    width: calc(40% - 15px);
+
+    .details__top {
+      background: #fff;
+      border-radius: 8px;
+      padding: 64px 16px;
+
+      svg {
+        width: 75px;
+        margin: 0 auto;
+      }
+    }
+  }
+}
+</style>
---
+++ pages/detalhes-equipamento.vue
@@ -0,0 +1,232 @@
+<template>
+  <section class="details">
+    <div class="container">
+      <div class="details__left">
+        <button><OutlineChevronLeftIcon /> Voltar para os equipamentos</button>
+        <SwiperGaleryThumb />
+      </div>
+
+      <div class="details__right">
+        <div class="details__top">
+          <small>Microfone</small>
+          <h2>Título do equipamento</h2>
+          <caption>
+            Nome do fornecedor
+          </caption>
+
+          <div class="details__price">
+            <caption>
+              Valor médio
+            </caption>
+            <div class="details__price-value">
+              <p>R$</p>
+              <h3>90,00</h3>
+            </div>
+          </div>
+
+          <a-button type="primary">Fazer orçamento</a-button>
+        </div>
+
+        <div class="details__bottom">
+          <div class="details__tab">
+            <button @click="showDescription = !showDescription">
+              Descrição <OutlineChevronDownIcon />
+            </button>
+            <transition name="fadeBottom" mode="out-in">
+              <div class="details__tab-description" v-if="showDescription">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Dignissimos impedit consequatur vero blanditiis vitae
+                  recusandae repudiandae ea accusantium soluta ipsa repellendus
+                  atque eligendi dolor voluptatibus odit, sed rerum corporis
+                  rem.
+                </p>
+              </div>
+            </transition>
+          </div>
+
+          <div class="details__tab">
+            <button @click="showEspecifications = !showEspecifications">
+              Especificações técnicas <OutlineChevronDownIcon />
+            </button>
+            <transition name="fadeBottom" mode="out-in">
+              <div
+                class="details__tab-especifications"
+                v-if="showEspecifications"
+              >
+                <ul>
+                  <li>
+                    • Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  </li>
+                  <li>
+                    • Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  </li>
+                  <li>
+                    • Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  </li>
+                  <li>
+                    • Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  </li>
+                  <li>
+                    • Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  </li>
+                </ul>
+              </div>
+            </transition>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  layout: "notLogged",
+  data() {
+    return {
+      showDescription: false,
+      showEspecifications: false,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.details {
+  margin-bottom: 64px;
+  .container {
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
+  }
+
+  .details__left {
+    width: calc(60% - 15px);
+    button {
+      display: flex;
+      align-items: center;
+      border: 0;
+      background: none;
+      color: $primary-yellow;
+      cursor: pointer;
+      padding: 8px 0;
+      margin: 16px 0;
+
+      svg {
+        width: 24px;
+        margin-right: 8px;
+      }
+    }
+  }
+
+  .details__right {
+    margin-top: 40px;
+    width: calc(40% - 15px);
+
+    .details__top {
+      background: #fff;
+      padding: 64px 16px;
+      small {
+        font-weight: bold;
+        color: $primary-yellow;
+        font-size: 0.875rem;
+      }
+
+      h2 {
+        font-size: 1.125rem;
+        font-weight: bold;
+        margin-bottom: 8px;
+      }
+
+      caption {
+        display: flex;
+        color: $border-gray;
+        padding: 0;
+        width: 100%;
+      }
+
+      .details__price {
+        margin-top: 16px;
+        margin-bottom: 32px;
+
+        .details__price-value {
+          display: flex;
+          align-items: flex-start;
+
+          p {
+            font-size: 0.875rem;
+            font-weight: bold;
+            color: $neutral-gray;
+            margin-right: 4px;
+          }
+
+          h3 {
+            font-size: 1.625rem;
+            line-height: 24px;
+            font-weight: bold;
+            color: $primary-yellow;
+            margin-bottom: 0;
+          }
+        }
+      }
+
+      button {
+        width: 100%;
+        font-weight: bold;
+      }
+    }
+
+    .details__bottom {
+      margin-top: 32px;
+      background: #fff;
+      // padding: 16px 0;
+
+      .details__tab {
+        &:nth-child(1) {
+          border-bottom: 1px solid $background-gray;
+        }
+
+        button {
+          width: 100%;
+          display: flex;
+          justify-content: space-between;
+          color: $primary-yellow;
+          padding: 4px 0;
+          border: 0;
+          background: none;
+          font-size: 1rem;
+          padding: 16px;
+          cursor: pointer;
+          font-weight: bold;
+
+          svg {
+            width: 20px;
+          }
+        }
+
+        .details__tab-description,
+        .details__tab-especifications {
+          padding: 16px;
+        }
+
+        p,
+        li {
+          font-size: 0.875rem;
+        }
+      }
+    }
+  }
+
+  @media (max-width: 1024px) {
+    .container {
+      flex-direction: column;
+    }
+
+    .details__left,
+    .details__right {
+      width: 100%;
+    }
+  }
+}
+</style>
