9061cff08013046090a44455d8f7d996badff900
vitorcosta0399 <vitor@nerau.com.br>
2021-09-23 20:24:47 -0300

    Header e footer  mobile

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -3,24 +3,22 @@
     <div class="footer">
       <div class="container footer__wrapper">
         <div class="footer__contato">
-          <h2 class="footer__title">
-            Como falar com a gente?
-          </h2>
+          <h2 class="footer__title">Como falar com a gente?</h2>
           <div class="footer__contato-wrapper">
             <div class="footer__contato-wrapper--left">
               <div class="footer__contato-item">
-                <img src="~/assets/img/icons/whatsapp.svg" alt="whatsapp">
+                <img src="~/assets/img/icons/whatsapp.svg" alt="whatsapp" />
                 <p>(11) 97070-7070</p>
               </div>
               <div class="footer__contato-item">
-                <img src="~/assets/img/icons/mail.svg" alt="e-mail">
+                <img src="~/assets/img/icons/mail.svg" alt="e-mail" />
                 <p>contato@lincer.com.br</p>
               </div>
               <div class="footer__contato-item">
-                <img src="~/assets/img/icons/localization.svg" alt="whatsapp">
+                <img src="~/assets/img/icons/localization.svg" alt="whatsapp" />
                 <p>
-                  Rua 1500, 820 - 2401<br>
-                  Centro, São Paulo-SP<br>
+                  Rua 1500, 820 - 2401<br />
+                  Centro, São Paulo-SP<br />
                   03535-000
                 </p>
               </div>
@@ -28,7 +26,7 @@
 
             <div class="footer__contato-wrapper-right">
               <div class="footer__contato-item">
-                <img src="~/assets/img/icons/phone.svg" alt="whatsapp">
+                <img src="~/assets/img/icons/phone.svg" alt="whatsapp" />
                 <p>(11) 7070-7070</p>
               </div>
             </div>
@@ -36,26 +34,44 @@
         </div>
 
         <div class="footer__newsletter">
-          <h2 class="footer__title">
-            Receba nossas novidades
-          </h2>
+          <h2 class="footer__title">Receba nossas novidades</h2>
 
-          <a-input v-model="emailNewsletter" class="primary" placeholder="Digite o seu melhor e-mail :)" />
-          <a-button type="secondary" class="btn-secondary">
-            Enviar
-          </a-button>
+          <a-input
+            v-model="emailNewsletter"
+            class="primary"
+            placeholder="Digite o seu melhor e-mail :)"
+          />
+          <a-button type="secondary" class="btn-secondary"> Enviar </a-button>
         </div>
 
         <div class="footer__social">
-          <h2 class="footer__title">
-            Nossas redes sociais
-          </h2>
+          <h2 class="footer__title">Nossas redes sociais</h2>
 
           <div class="footer__social-icons">
-            <a href="#"><img class="icon-media" src="~/assets/img/icons/facebook.svg" alt="facebook"></a>
-            <a href="#"><img class="icon-media" src="~/assets/img/icons/youtube.svg" alt="youtube"></a>
-            <a href="#"><img class="icon-media" src="~/assets/img/icons/instagram.svg" alt="instagram"></a>
-            <a href="#"><img class="icon-media" src="~/assets/img/icons/linkedin.svg" alt="linkedin"></a>
+            <a href="#"
+              ><img
+                class="icon-media"
+                src="~/assets/img/icons/facebook.svg"
+                alt="facebook"
+            /></a>
+            <a href="#"
+              ><img
+                class="icon-media"
+                src="~/assets/img/icons/youtube.svg"
+                alt="youtube"
+            /></a>
+            <a href="#"
+              ><img
+                class="icon-media"
+                src="~/assets/img/icons/instagram.svg"
+                alt="instagram"
+            /></a>
+            <a href="#"
+              ><img
+                class="icon-media"
+                src="~/assets/img/icons/linkedin.svg"
+                alt="linkedin"
+            /></a>
           </div>
         </div>
       </div>
@@ -70,84 +86,105 @@
 
 <script>
 export default {
-  name: 'Footer',
-  data () {
+  name: "Footer",
+  data() {
     return {
-      emailNewsletter: null
-    }
-  }
-}
+      emailNewsletter: null,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  .container {
-    max-width: 1242px;
-    margin: 0 auto;
+.container {
+  max-width: 1242px;
+  margin: 0 auto;
+}
+
+.footer {
+  background: $secondary-dark;
+  padding: 43px 0;
+
+  &__wrapper {
+    display: flex;
+    justify-content: space-between;
+  }
+  &__title {
+    font-size: 1.125rem;
+    color: $neutral-white;
+    font-weight: 500;
+    margin-bottom: 30px;
   }
 
-  .footer{
-    background: $secondary-dark;
-    padding: 43px 0;
-
-    &__wrapper{
+  &__contato {
+    &-wrapper {
       display: flex;
-      justify-content: space-between;
-    }
-    &__title{
-      font-size: 1.125rem;
-      color: $neutral-white;
-      font-weight: 500;
-      margin-bottom: 30px;
+      column-gap: 20px;
     }
 
-    &__contato{
-      &-wrapper{
-        display: flex;
-        column-gap: 20px;
-      }
+    &-item {
+      display: flex;
+      align-items: center;
+      column-gap: 5px;
+      margin-bottom: 20px;
 
-      &-item{
-        display: flex;
-        align-items: center;
-        column-gap: 5px;
-        margin-bottom: 20px;
-
-        p{
-          margin-bottom: 0;
-          font-size: 0.875rem;
-          color: $neutral-white;
-          font-weight: 400;
-          font-family: $font-family-roboto;
-        }
+      p {
+        margin-bottom: 0;
+        font-size: 0.875rem;
+        color: $neutral-white;
+        font-weight: 400;
+        font-family: $font-family-roboto;
       }
     }
+  }
 
-    &__social{
-      &-icons{
-        display: flex;
-        align-items: center;
-        column-gap: 20px;
+  &__social {
+    &-icons {
+      display: flex;
+      align-items: center;
+      column-gap: 20px;
 
-        .icon-media{
-          transition: .3s transform;
+      .icon-media {
+        transition: 0.3s transform;
 
-          &:hover{
-            transform: scale(2);
-          }
+        &:hover {
+          transform: scale(2);
         }
       }
     }
   }
 
-  .copyright{
-    background: $background-gray !important;
-    height: 80px;
-    padding-top: 35px;
+  @media (max-width: 1024px) {
+    &__wrapper {
+      flex-direction: column;
 
-    p{
-      color: $neutral-dark;
-      font-family: $font-family-roboto;
-      font-weight: 500;
+      .footer__newsletter {
+        margin: 32px 0;
+        max-width: 380px;
+      }
+    }
+
+    &__title {
+      margin-bottom: 16px;
+    }
+
+    &__contato {
+      &-wrapper {
+        flex-direction: column;
+      }
     }
   }
+}
+
+.copyright {
+  background: $background-gray !important;
+  height: 80px;
+  padding-top: 35px;
+
+  p {
+    color: $neutral-dark;
+    font-family: $font-family-roboto;
+    font-weight: 500;
+  }
+}
 </style>
---
+++ pages/equipamentos/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>aoudaiouhdsuiy</h1>
+</template>
+
+<script>
+export default {
+  layout: "notLogged",
+};
+</script>
+
+<style>
+</style>
