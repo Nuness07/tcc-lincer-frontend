f144bc4855bfef17650bee75d0dbfacd8184dad2
vitorcosta0399 <vitor@nerau.com.br>
2021-10-20 22:08:52 -0300

    aprovado

---
+++ components/Dashboard/Freelancer/GuiasDicas.vue
@@ -0,0 +1,82 @@
+<template>
+  <section class="guias">
+    <div class="guias__header">
+      <h2>Guias e dicas úteis</h2>
+      <p>
+        Dicas e guias para gerar mais engajamento nos seus cursos publicados
+      </p>
+    </div>
+
+    <div class="guias__body">
+      <GuiasDicasCard />
+      <GuiasDicasCard />
+      <GuiasDicasCard />
+      <GuiasDicasCard />
+    </div>
+  </section>
+</template>
+
+<script>
+import GuiasDicasCard from "./GuiasDicasCard.vue";
+export default {
+  components: { GuiasDicasCard },
+};
+</script>
+
+<style lang="scss" scoped>
+.guias {
+  .guias__header {
+    margin-bottom: 16px;
+    h2 {
+      font-size: 1.5rem;
+      color: #ffb400;
+      font-weight: bold;
+      margin-bottom: 0;
+    }
+  }
+
+  .guias__body {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    .card {
+      width: calc(100% / 4 - 10px);
+    }
+
+    @media (max-width: 1200px) {
+      .card {
+        width: calc(100% / 3 - 10px);
+
+        &:nth-child(4) {
+          margin-top: 20px;
+        }
+      }
+    }
+
+    @media (max-width: 1024px) {
+      .card {
+        width: calc(100% / 2 - 10px);
+
+        &:nth-child(3) {
+          margin-top: 20px;
+        }
+      }
+    }
+
+    @media (max-width: 576px) {
+      .card {
+        width: 100%;
+
+        &:nth-child(3),
+        &:nth-child(4) {
+          margin-top: initial;
+        }
+
+        &:not(:last-child) {
+          margin-bottom: 20px;
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ components/Dashboard/Freelancer/GuiasDicasCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="card">
+    <picture>
+      <img src="@/assets/img/banner-bg.png" alt="asd" />
+    </picture>
+    <h3>Dicas After Efects</h3>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate
+      egestas imperdiet. Donec felis erat, maximus ut viverra.Etiam...
+    </p>
+    <a-button type="primary">Acessar</a-button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  picture {
+    display: flex;
+    align-items: center;
+    padding-top: 56.25%;
+    position: relative;
+    overflow: hidden;
+
+    img {
+      position: absolute;
+      left: 0;
+      top: 0;
+      max-width: 100%;
+      width: 100%;
+    }
+  }
+
+  h3 {
+    font-size: 1.2rem;
+    line-height: 24px;
+    margin: 8px 0;
+  }
+
+  p {
+    font-size: 0.875rem;
+    margin-bottom: 16px !important;
+  }
+
+  .ant-btn {
+      width: 100%;
+  }
+}
+</style>
